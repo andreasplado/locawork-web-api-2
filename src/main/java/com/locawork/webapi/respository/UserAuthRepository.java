ebb1f8e4fb65b55e8064a119333b3af71bc190fb
Andreas Plado <plado.andreas@hotmail.com>
2023-05-28 18:35:11 +0300

    Refactoring

--- src/main/java/com/locawork/webapi/respository/UserAuthRepository.java
+++ src/main/java/com/locawork/webapi/respository/UserAuthRepository.java
@@ -11,7 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 public interface UserAuthRepository extends JpaRepository<UserEntity, String> {
 
-    @Query(value="SELECT CASE WHEN COUNT(s)> 0 then true else false end FROM users u WHERE u.username=?1 AND u.password=?2", nativeQuery = true)
+    @Query(value="SELECT CASE WHEN COUNT(s)> 0 then true else false end FROM users u WHERE u.email=?1 AND u.password=?2", nativeQuery = true)
     boolean isAuthenticated(@Param("username") String username, @Param("password") String password);
 
     @Query(value="SELECT * FROM users u WHERE u.username=?1", nativeQuery = true)
