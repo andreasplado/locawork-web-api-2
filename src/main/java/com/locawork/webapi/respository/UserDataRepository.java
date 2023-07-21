7e721eda0c48bce76f843e1cc126dd7fa83c9106
Andreas Plado <plado.andreas@hotmail.com>
2023-07-21 20:20:56 +0300

    Refactoring

--- src/main/java/com/locawork/webapi/respository/UserDataRepository.java
+++ src/main/java/com/locawork/webapi/respository/UserDataRepository.java
@@ -52,7 +52,7 @@ public interface UserDataRepository extends JpaRepository<UserEntity, Integer> {
     @Query(value="SELECT u.password FROM users u WHERE u.email=?1", nativeQuery = true)
     String getUserPassword(@Param("id") String username);
 
-    @Query(value="SELECT COUNT(u.id) = 1 FROM users u WHERE u.email= :email AND u.password= :password", nativeQuery = true)
+    @Query(value="SELECT CASE WHEN COUNT(u)> 0 then true else FROM users u WHERE u.email=?1 AND u.password=?2", nativeQuery = true)
     boolean userAuthenticated(@Param("email") String email, @Param("password") String password);
 
 }
