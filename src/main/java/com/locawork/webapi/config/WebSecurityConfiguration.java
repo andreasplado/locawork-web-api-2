9ea72daff634fa88a4c77807e88bbd88be0c33b8
Andreas Plado <plado.andreas@hotmail.com>
2023-08-17 13:13:08 +0300

    Refactoring

--- src/main/java/com/locawork/webapi/config/WebSecurityConfiguration.java
+++ src/main/java/com/locawork/webapi/config/WebSecurityConfiguration.java
@@ -10,6 +10,8 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableWebSecurity
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
@@ -46,4 +48,16 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .userDetailsService(userDetailsService)
                 .passwordEncoder(bCryptPasswordEncoder);
     }
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedMethods("OPTIONS", "HEAD", "GET", "PUT", "POST", "DELETE")
+                        .allowedOrigins("*");
+            }
+        };
+    }
+
 }
