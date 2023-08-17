74964a9ac9527a2ac42ea6b981a92cf8791fa9f9
Andreas Plado <plado.andreas@hotmail.com>
2023-08-17 20:03:53 +0300

    Refactoring

--- src/main/java/com/locawork/webapi/config/AuthorizationFilter.java
+++ src/main/java/com/locawork/webapi/config/AuthorizationFilter.java
@@ -31,6 +31,7 @@ public class AuthorizationFilter extends BasicAuthenticationFilter  {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws IOException, ServletException {
         String header = request.getHeader("Authorization");
+        System.out.println(header);
         if (header == null || !header.startsWith("Bearer")) {
             filterChain.doFilter(request, response);
 
