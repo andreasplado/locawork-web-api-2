d53cb131996b111b107eba83ff22333dc4190024
Andreas Plado <plado.andreas@hotmail.com>
2023-08-17 19:12:11 +0300

    Refactoring

--- src/main/java/com/locawork/webapi/config/AuthorizationFilter.java
+++ src/main/java/com/locawork/webapi/config/AuthorizationFilter.java
@@ -7,6 +7,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.stereotype.Component;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
@@ -16,7 +17,7 @@ import java.io.IOException;
 import java.nio.charset.Charset;
 import java.util.ArrayList;
 
-
+@Component
 public class AuthorizationFilter extends BasicAuthenticationFilter  {
     @Value("${jwt.secret}")
     private String secret;
