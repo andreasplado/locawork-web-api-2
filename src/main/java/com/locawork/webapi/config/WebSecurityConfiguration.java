ae2958b4b14719a00d19b67c71ff07b92c081483
Andreas Plado <plado.andreas@hotmail.com>
2023-08-31 19:05:41 +0300

    Refactoring

--- src/main/java/com/locawork/webapi/config/WebSecurityConfiguration.java
+++ src/main/java/com/locawork/webapi/config/WebSecurityConfiguration.java
@@ -55,6 +55,7 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
         web.ignoring().regexMatchers("/users/signup");
         web.ignoring().regexMatchers("/auth/authenticate");
         web.ignoring().regexMatchers("/docs/privacy-policy");
+        web.ignoring().regexMatchers("/comments*");
     }
 
     @Override
